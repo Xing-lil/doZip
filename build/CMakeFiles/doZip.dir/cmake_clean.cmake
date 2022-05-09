ad916c183f560e1c7977eecba1ca2f2bc6ba421a
Xing-lil <xing@njust.edu.cn>
2022-05-09 10:12:56 +0800

    first commit

---
+++ build/CMakeFiles/doZip.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doZip.dir/src/miniz.c.o"
+  "libdoZip.pdb"
+  "libdoZip.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/doZip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
